e9b9671aa0f82a086f1b37b352f6f27996d37c3e
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2021-07-25 13:34:52 +0530

    Addd a onClickListener

--- app/src/main/java/com/example/android/onclicklistenerandcustomarrayadapter/MainActivity.java
+++ app/src/main/java/com/example/android/onclicklistenerandcustomarrayadapter/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Create a  TextView variable t1
+        TextView t1 = (TextView) findViewById(R.id.display);
+
+        //Set a onClickListener on t1
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            //Create  a onClick method
+            public void onClick(View v) {
+
+                //Create a Explicit Intent to open a new Activity2
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
